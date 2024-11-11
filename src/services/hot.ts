91b88280df0a614045643f72d8c9c3eca4c02e4b
yuuuuuj <17373046129@163.com>
2024-11-11 20:16:39 +0800

    init(custom): 热门推荐数据列表

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,11 +1,11 @@
 import { http } from '@/utils/http'
-import type { IHotPage, IHotPageItem } from '@/types/hot'
+import type { IHotPage, IHotRes } from '@/types/hot'
 
 // 热门推荐详细数据
 export const getHotAPI = (url: string, data: IHotPage) => {
-  return http<IList<IHotPageItem>>({
+  return http<IHotRes>({
     method: 'GET',
-    url: '/home/goods/guessLike',
+    url: url,
     data,
   })
 }
